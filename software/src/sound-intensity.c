7f837b780d848f6498144fe796af5ebc8c92877a
Matthias Bolte <matthias@tinkerforge.com>
2014-01-06 12:18:29 +0100

    Add static assert for BrickContext size

--- software/src/sound-intensity.c
+++ software/src/sound-intensity.c
@@ -56,6 +56,8 @@ void invocation(const ComType com, const uint8_t *data) {
 }
 
 void constructor(void) {
+	_Static_assert(sizeof(BrickContext) <= BRICKLET_CONTEXT_MAX_SIZE, "BrickContext too big");
+
 	adc_channel_enable(BS->adc_channel);
 	simple_constructor();
 }
