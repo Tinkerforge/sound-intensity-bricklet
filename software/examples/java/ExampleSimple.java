2747ad0d4cc9c7a58a8cefb0cab4e6c48920ec58
Matthias Bolte <matthias@tinkerforge.com>
2014-04-02 17:30:46 +0200

    java: Unify constants style

--- software/examples/java/ExampleSimple.java
+++ software/examples/java/ExampleSimple.java
@@ -2,17 +2,17 @@ import com.tinkerforge.BrickletSoundIntensity;
 import com.tinkerforge.IPConnection;
 
 public class ExampleSimple {
-	private static final String host = "localhost";
-	private static final int port = 4223;
+	private static final String HOST = "localhost";
+	private static final int PORT = 4223;
 	private static final String UID = "XYZ"; // Change to your UID
-	
+
 	// Note: To make the example code cleaner we do not handle exceptions. Exceptions you
 	//       might normally want to catch are described in the documentation
 	public static void main(String args[]) throws Exception {
 		IPConnection ipcon = new IPConnection(); // Create IP connection
 		BrickletSoundIntensity si = new BrickletSoundIntensity(UID, ipcon); // Create device object
 
-		ipcon.connect(host, port); // Connect to brickd
+		ipcon.connect(HOST, PORT); // Connect to brickd
 		// Don't use device before ipcon is connected
 
 		// Get current intensity
