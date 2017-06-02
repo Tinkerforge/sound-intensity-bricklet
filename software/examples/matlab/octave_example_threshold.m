2428596e063e9d3765cd2c6a649754dedb5d5555
Matthias Bolte <matthias@tinkerforge.com>
2017-06-02 16:27:03 +0200

    octave: Use javaObject instead of deprecated java_new

--- software/examples/matlab/octave_example_threshold.m
+++ software/examples/matlab/octave_example_threshold.m
@@ -5,8 +5,8 @@ function octave_example_threshold()
     PORT = 4223;
     UID = "XYZ"; % Change XYZ to the UID of your Sound Intensity Bricklet
 
-    ipcon = java_new("com.tinkerforge.IPConnection"); % Create IP connection
-    si = java_new("com.tinkerforge.BrickletSoundIntensity", UID, ipcon); % Create device object
+    ipcon = javaObject("com.tinkerforge.IPConnection"); % Create IP connection
+    si = javaObject("com.tinkerforge.BrickletSoundIntensity", UID, ipcon); % Create device object
 
     ipcon.connect(HOST, PORT); % Connect to brickd
     % Don't use device before ipcon is connected
