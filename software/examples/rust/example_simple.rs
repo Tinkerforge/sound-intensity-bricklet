5950871d12d33b6f103875b7fc9505978d42000a
Erik Fleckstein <erik@tinkerforge.com>
2018-11-07 16:27:51 +0100

    rust: Accept host names for ip connection, fix naming inconsistencies

--- software/examples/rust/example_simple.rs
+++ software/examples/rust/example_simple.rs
@@ -1,20 +1,20 @@
 use std::{error::Error, io};
 
-use tinkerforge::{ipconnection::IpConnection, sound_intensity_bricklet::*};
+use tinkerforge::{ip_connection::IpConnection, sound_intensity_bricklet::*};
 
-const HOST: &str = "127.0.0.1";
+const HOST: &str = "localhost";
 const PORT: u16 = 4223;
-const UID: &str = "XYZ"; // Change XYZ to the UID of your Sound Intensity Bricklet
+const UID: &str = "XYZ"; // Change XYZ to the UID of your Sound Intensity Bricklet.
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let ipcon = IpConnection::new(); // Create IP connection
-    let sound_intensity_bricklet = SoundIntensityBricklet::new(UID, &ipcon); // Create device object
+    let ipcon = IpConnection::new(); // Create IP connection.
+    let si = SoundIntensityBricklet::new(UID, &ipcon); // Create device object.
 
-    ipcon.connect(HOST, PORT).recv()??; // Connect to brickd
-                                        // Don't use device before ipcon is connected
+    ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
+                                          // Don't use device before ipcon is connected.
 
-    // Get current intensity
-    let intensity = sound_intensity_bricklet.get_intensity().recv()?;
+    // Get current intensity.
+    let intensity = si.get_intensity().recv()?;
     println!("Intensity: {}", intensity);
 
     println!("Press enter to exit.");
--- software/examples/rust/example_threshold.rs
+++ software/examples/rust/example_threshold.rs
@@ -1,32 +1,33 @@
 use std::{error::Error, io, thread};
-use tinkerforge::{ipconnection::IpConnection, sound_intensity_bricklet::*};
+use tinkerforge::{ip_connection::IpConnection, sound_intensity_bricklet::*};
 
-const HOST: &str = "127.0.0.1";
+const HOST: &str = "localhost";
 const PORT: u16 = 4223;
-const UID: &str = "XYZ"; // Change XYZ to the UID of your Sound Intensity Bricklet
+const UID: &str = "XYZ"; // Change XYZ to the UID of your Sound Intensity Bricklet.
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let ipcon = IpConnection::new(); // Create IP connection
-    let sound_intensity_bricklet = SoundIntensityBricklet::new(UID, &ipcon); // Create device object
+    let ipcon = IpConnection::new(); // Create IP connection.
+    let si = SoundIntensityBricklet::new(UID, &ipcon); // Create device object.
 
-    ipcon.connect(HOST, PORT).recv()??; // Connect to brickd
-                                        // Don't use device before ipcon is connected
+    ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
+                                          // Don't use device before ipcon is connected.
 
-    // Get threshold listeners with a debounce time of 1 second (1000ms)
-    sound_intensity_bricklet.set_debounce_period(1000);
+    // Get threshold receivers with a debounce time of 1 second (1000ms).
+    si.set_debounce_period(1000);
 
-    //Create listener for intensity reached events.
-    let intensity_reached_listener = sound_intensity_bricklet.get_intensity_reached_receiver();
-    // Spawn thread to handle received events. This thread ends when the sound_intensity_bricklet
+    // Create receiver for intensity reached events.
+    let intensity_reached_receiver = si.get_intensity_reached_receiver();
+
+    // Spawn thread to handle received events. This thread ends when the `si` object
     // is dropped, so there is no need for manual cleanup.
     thread::spawn(move || {
-        for event in intensity_reached_listener {
-            println!("Intensity: {}", event);
+        for intensity_reached in intensity_reached_receiver {
+            println!("Intensity: {}", intensity_reached);
         }
     });
 
-    // Configure threshold for intensity "greater than 2000"
-    sound_intensity_bricklet.set_intensity_callback_threshold('>', 2000, 0);
+    // Configure threshold for intensity "greater than 2000".
+    si.set_intensity_callback_threshold('>', 2000, 0);
 
     println!("Press enter to exit.");
     let mut _input = String::new();
