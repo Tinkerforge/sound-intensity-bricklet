ffbb7d19538d9a7e9bfe94c6d725f68fe2e0e07e
Erik Fleckstein <erik@tinkerforge.com>
2018-12-20 16:41:03 +0100

    rust: Reformat examples.

--- software/examples/rust/example_callback.rs
+++ software/examples/rust/example_callback.rs
@@ -1,5 +1,8 @@
-use std::{error::Error, io, thread};
-use tinkerforge::{ip_connection::IpConnection, sound_intensity_bricklet::*};
+use std::{io, error::Error};
+use std::thread;
+use tinkerforge::{ip_connection::IpConnection, 
+                  sound_intensity_bricklet::*};
+
 
 const HOST: &str = "localhost";
 const PORT: u16 = 4223;
@@ -10,23 +13,23 @@ fn main() -> Result<(), Box<dyn Error>> {
     let si = SoundIntensityBricklet::new(UID, &ipcon); // Create device object.
 
     ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
-                                          // Don't use device before ipcon is connected.
+    // Don't use device before ipcon is connected.
 
-    let intensity_receiver = si.get_intensity_callback_receiver();
+     let intensity_receiver = si.get_intensity_callback_receiver();
 
-    // Spawn thread to handle received callback messages.
-    // This thread ends when the `si` object
-    // is dropped, so there is no need for manual cleanup.
-    thread::spawn(move || {
-        for intensity in intensity_receiver {
-            println!("Intensity: {}", intensity);
-        }
-    });
+        // Spawn thread to handle received callback messages. 
+        // This thread ends when the `si` object
+        // is dropped, so there is no need for manual cleanup.
+        thread::spawn(move || {
+            for intensity in intensity_receiver {           
+                		println!("Intensity: {}", intensity);
+            }
+        });
 
-    // Set period for intensity receiver to 0.05s (50ms).
-    // Note: The intensity callback is only called every 0.05 seconds
-    //       if the intensity has changed since the last call!
-    si.set_intensity_callback_period(50);
+		// Set period for intensity receiver to 0.05s (50ms).
+		// Note: The intensity callback is only called every 0.05 seconds
+		//       if the intensity has changed since the last call!
+		si.set_intensity_callback_period(50);
 
     println!("Press enter to exit.");
     let mut _input = String::new();
--- software/examples/rust/example_threshold.rs
+++ software/examples/rust/example_threshold.rs
@@ -1,5 +1,8 @@
-use std::{error::Error, io, thread};
-use tinkerforge::{ip_connection::IpConnection, sound_intensity_bricklet::*};
+use std::{io, error::Error};
+use std::thread;
+use tinkerforge::{ip_connection::IpConnection, 
+                  sound_intensity_bricklet::*};
+
 
 const HOST: &str = "localhost";
 const PORT: u16 = 4223;
@@ -10,24 +13,24 @@ fn main() -> Result<(), Box<dyn Error>> {
     let si = SoundIntensityBricklet::new(UID, &ipcon); // Create device object.
 
     ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
-                                          // Don't use device before ipcon is connected.
+    // Don't use device before ipcon is connected.
 
-    // Get threshold receivers with a debounce time of 1 second (1000ms).
-    si.set_debounce_period(1000);
+		// Get threshold receivers with a debounce time of 1 second (1000ms).
+		si.set_debounce_period(1000);
 
-    let intensity_reached_receiver = si.get_intensity_reached_callback_receiver();
+     let intensity_reached_receiver = si.get_intensity_reached_callback_receiver();
 
-    // Spawn thread to handle received callback messages.
-    // This thread ends when the `si` object
-    // is dropped, so there is no need for manual cleanup.
-    thread::spawn(move || {
-        for intensity_reached in intensity_reached_receiver {
-            println!("Intensity: {}", intensity_reached);
-        }
-    });
+        // Spawn thread to handle received callback messages. 
+        // This thread ends when the `si` object
+        // is dropped, so there is no need for manual cleanup.
+        thread::spawn(move || {
+            for intensity_reached in intensity_reached_receiver {           
+                		println!("Intensity: {}", intensity_reached);
+            }
+        });
 
-    // Configure threshold for intensity "greater than 2000".
-    si.set_intensity_callback_threshold('>', 2000, 0);
+		// Configure threshold for intensity "greater than 2000".
+		si.set_intensity_callback_threshold('>', 2000, 0);
 
     println!("Press enter to exit.");
     let mut _input = String::new();
